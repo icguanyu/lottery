589ac2c5e9de8d580f83861b7c44eadda94b333a
icguanyu <icguanyu@gmail.com>
2018-10-17 18:31:55 +0800

    result layout done

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -7,8 +7,8 @@
       <div class="ticket">
         <div class="logo">
           <img src="@/assets/img/lottodemo-03.png" alt="">
-          <div class="code">0001 1010 1100 0111 0110 1110 0011</div>
         </div>
+        <div class="code">0001 1010 1100 0111 0110 1110 0011</div>
         <div class="col">
           <div class="title">★ 銷售日期 ★</div>
           <div class="info">{{date}}</div>
@@ -38,7 +38,48 @@
         </div>
         <div class="unicode"></div>
       </div>
-      <div class="result"></div>
+      <div class="result">
+        <div class="display">
+          <div class="number">00</div>
+          <div class="number">00</div>
+          <div class="number">00</div>
+          <div class="number">00</div>
+          <div class="number">00</div>
+          <div class="number">00</div>
+        </div>
+        <div class="output">
+          <div class="period">
+            <p>第00000001期</p>
+            <img src="@/assets/img/lottodemo-03.png" alt="">
+          </div>
+          <div class="output_number_box">
+            <div class="output_number"><span>00</span></div>
+            <div class="output_number"><span>00</span></div>
+            <div class="output_number"><span>00</span></div>
+            <div class="output_number"><span>00</span></div>
+            <div class="output_number"><span>00</span></div>
+            <div class="output_number"><span>00</span></div>
+          </div>
+        </div>
+        <div class="operation">
+          <div class="order item">
+            <div class="title">★ 排序方式</div>
+            <div class="sort">
+              <input type="radio" id="one" value="one" name="radio">
+              <label for="one">由小到大</label>
+              <input type="radio" id="two" value="two" name="radio">
+              <label for="two">開獎順序</label>
+            </div>
+          </div>
+          <div class="switch item">
+            <div class="title">★ 操作</div>
+            <div class="btn">
+              <div class="pause">暫停</div>
+              <div class="start">開獎</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -47,23 +88,21 @@
 const axios = require("axios");
 export default {
   name: "Result",
-  methods: {
-  },
+  methods: {},
   computed: {
     selected() {
-      return this.$store.state.selected
+      return this.$store.state.selected;
     },
     date() {
-      return this.$store.state.date
+      return this.$store.state.date;
     }
   },
-  created(){
+  created() {
     //避免出現沒選號碼的結果,自動導回首頁
-    let selected = this.$store.state.selected.length
-    if( selected < 6) {
-      this.$router.push('/')
+    let selected = this.$store.state.selected.length;
+    if (selected < 6) {
+      this.$router.push("/");
     }
-    
   }
 };
 </script>
